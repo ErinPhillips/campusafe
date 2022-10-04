de145dd44a5dbb2b1d3d3d7854c516b034e23089
meghangillikin <meghangillikin@gmail.com>
2022-10-04 16:41:09 -0400

    c

--- public/contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-if($_POST["Message"]) {
-mail("meghangillikin@gmail.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: gillikinme@g.cofc.edu");
-}
-?>
--- public/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$data = &_GET["login"];
-    
-
-?>
